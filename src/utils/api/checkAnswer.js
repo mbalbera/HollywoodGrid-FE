778396deb65136b5b1fbb926b423d74a8a400ed0
mbalbera <50467671+mbalbera@users.noreply.github.com>
2024-03-07 19:31:50 -0700

    fix commented lines

--- src/utils/api/checkAnswer.js
+++ src/utils/api/checkAnswer.js
@@ -8,3 +8,4 @@ const checkAnswer = async({cat1, cat2, actorId}) => {
     // return apiHelper('/api/check-answer', 'POST', body, true)
     return {status:'success', guessStatus: 'correct', actor: {name: 'Tom Cruise', id: 0}}
 }
+export default checkAnswer
